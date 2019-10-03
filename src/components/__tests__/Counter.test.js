7baf6f1ca6ae728314bf2fa6cd70baddfefc19d8
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-03 19:00:33 +0100

    prevents the count from going under a lower limit test

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import * as rtl from '@testing-library/react';
-import Counter from '../Counter';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import * as rtl from "@testing-library/react";
+import Counter from "../Counter";
 
 let tools;
 
 beforeEach(() => {
   rtl.cleanup();
-  tools = rtl.render(<Counter user='Peter' countLimit='5'/>);
+  tools = rtl.render(<Counter user="Peter" countLimit="5" />);
 });
 
-describe('Counter component', () => {
-  it('can debug the output', () => {
+describe("Counter component", () => {
+  it("can debug the output", () => {
     tools.debug();
   });
 
-  it('shows the correct user', () => {
+  it("shows the correct user", () => {
     const elementWithJoshText = tools.queryByText(/peter/i);
     expect(elementWithJoshText).toBeInTheDocument();
   });
 
-  it('initial count is zero', () => {
+  it("initial count is zero", () => {
     const elementWithZero = tools.queryByText(/0/);
     expect(elementWithZero).toBeInTheDocument();
   });
 
-  it('can increment the count by one by clicking increment', () => {
-    const incButton = tools.queryByTestId('incButton');
+  it("can increment the count by one by clicking increment", () => {
+    const incButton = tools.queryByTestId("incButton");
 
     rtl.fireEvent.click(incButton);
     expect(tools.queryByText(/0/)).not.toBeInTheDocument();
@@ -37,34 +37,40 @@ describe('Counter component', () => {
     expect(tools.queryByText(/2/)).toBeInTheDocument();
   });
 
-  it('can decrement the count by one by clicking decrement', () => {
-    const counterDecrease = tools.queryByTestId('decButton');
+  it("can decrement the count by one by clicking decrement", () => {
+    const counterDecrease = tools.queryByTestId("decButton");
     expect(counterDecrease).toBeInTheDocument();
   });
 
-  it('can reset the count clicking rest', () => {
-    const counterReset = tools.queryByTestId('resetButton');
-    expect(counterReset).toBeInTheDocument()
+  it("can reset the count clicking rest", () => {
+    const counterReset = tools.queryByTestId("resetButton");
+    expect(counterReset).toBeInTheDocument();
   });
 
-  it('prevents the count from going over an upper limit', () => {
-      const incButton = tools.queryByTestId('incButton');
-  
-      rtl.fireEvent.click(incButton);
-      rtl.fireEvent.click(incButton);
-      rtl.fireEvent.click(incButton);
-      rtl.fireEvent.click(incButton);
-      rtl.fireEvent.click(incButton);
+  it("prevents the count from going over an upper limit", () => {
+    const incButton = tools.queryByTestId("incButton");
+
+    rtl.fireEvent.click(incButton);
+    rtl.fireEvent.click(incButton);
+    rtl.fireEvent.click(incButton);
+    rtl.fireEvent.click(incButton);
+    rtl.fireEvent.click(incButton);
     const counterUpperLimit = tools.queryByText(/5/i);
-    expect(counterUpperLimit).toBeInTheDocument()
+    expect(counterUpperLimit).toBeInTheDocument();
   });
 
-  it('prevents the count from going under a lower limit', () => {
+  it("prevents the count from going under a lower limit", () => {
+    const decButton = tools.queryByTestId("decButton");
+    rtl.fireEvent.click(decButton);
+    rtl.fireEvent.click(decButton);
+    rtl.fireEvent.click(decButton);
+    rtl.fireEvent.click(decButton);
+    rtl.fireEvent.click(decButton);
+    const counterLowestLimit = tools.queryByText(/-5/i);
+    expect(counterLowestLimit).toBeInTheDocument();
   });
 
-  it('shows a warning once we hit the upper limit of the counter', () => {
-  });
+  it("shows a warning once we hit the upper limit of the counter", () => {});
 
-  it('shows a warning once we hit the lower limit of the counter', () => {
-  });
+  it("shows a warning once we hit the lower limit of the counter", () => {});
 });
